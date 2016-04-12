267ab5b9306299f8356a5b77e040dcb787433316
Teyras <teyras@gmail.com>
2016-04-12 13:28:07 +0200

    add a task index view

--- JBToDo/views.py
+++ JBToDo/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from JBToDo import app
+from JBToDo.model import Task
 
 @app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
+def index():
+    items = Task.query.all()
+    return render_template('show_items.html', items = items)
